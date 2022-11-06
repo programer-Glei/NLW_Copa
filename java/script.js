7607c5e7c58f5dd2fdb26f6c4237ec53d4e161dd
Gleibson Pereira <gleibson.santos05@gmail.com>
2022-11-06 18:34:06 -0300

    proximo comit

---
+++ java/script.js
@@ -0,0 +1,25 @@
+
+document.querySelector('#cards').innerHTML = `
+
+<div class="card">
+    <h2>24/11 <span>quinta</span></h2>
+    <ul>
+        <li>
+            <img src="img/brasil.svg" alt="">
+            <strong>16:00</strong>
+            <img src="img/camaroes.svg" alt="">
+        </li>
+    </ul>
+</div>
+
+<div class="card">
+    <h2>24/11 <span>quinta</span></h2>
+    <ul>
+        <li>
+            <img src="img/brasil.svg" alt="">
+            <strong>16:00</strong>
+            <img src="img/camaroes.svg" alt="">
+        </li>
+    </ul>
+</div>
+`
